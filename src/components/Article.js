c2f8b0a012284a5692e5d21ce40862b9c23b18d0
Aleksey Sereda <asereda@stratim.com>
2018-01-30 13:15:46 +0300

    add Articles

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,17 @@
-import React from 'react'
-function Article(props) {
-  const {artilce} = props
-  const body = <section>{artilce.text}</section>
-  return (
-      <div>
-        <h2>{artilce.title}</h2>
-        {body}
-        <h3>creation date: {(new Date(artilce.date)).toDateString()}</h3>
-      </div>
-  )
+import React, {Component} from 'react'
+
+class Article extends Component {
+  render() {
+    const {article} = this.props
+    const body = <section>{article.text}</section>
+    return (
+        <div>
+          <h2>{article.title}</h2>
+          {body}
+          <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+        </div>
+    )
+  }
 }
 
 export default Article
---
+++ src/fixtures.js
@@ -0,0 +1,49 @@
+export default [
+  {
+    "id": "435345345345345",
+    "date": "2016-06-09T15:03:23.000Z",
+    "title": "Commodo qui incididunt",
+    "text": "Commodo qui incididunt. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy",
+    "comments": [
+      {
+        "id": "qweqwq",
+        "user": "Galina Underwort",
+        "text": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like"
+      },
+      {
+        "id": "rtyry",
+        "user": "Iuliya Rtuisos",
+        "text": "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English"
+      },
+      {
+        "id": "cvxddssd",
+        "user": "Tison Iauau",
+        "text": "Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable"
+      }
+    ]
+  },
+  {
+    "id": "36456456456456",
+    "date": "2016-07-09T15:05:23.000Z",
+    "title": "It is a long established",
+    "text": "It is a long established desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy",
+    "comments": [
+      {
+        "id": "56753",
+        "user": "GSi. Underwort",
+        "text": "It  of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like"
+      },
+      {
+        "id": "563fddd",
+        "user": "IOps Hyuiis",
+        "text": "Of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English"
+      }
+    ]
+  },
+  {
+    "id": "435345345345345",
+    "date": "2016-06-09T15:03:23.000Z",
+    "title": "Commodo qui incididunt",
+    "text": "Commodo qui incididunt. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy"
+  }
+]
