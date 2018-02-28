43eeb0d632a09fa544324e76d82f4d36ec52877b
Aleksey Sereda <asereda@stratim.com>
2018-02-28 21:41:39 +0300

    Article button

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,16 +1,31 @@
 import React, {Component} from 'react'
 
 class Article extends Component {
+  state = {
+    isOpen: true
+  }
+
   render() {
     const {article} = this.props
-    const body = <section>{article.text}</section>
+    const body = this.state.isOpen && <section>{article.text}</section>
     return (
         <div>
-          <h2>{article.title}</h2>
+          <h2>{article.title}
+              <button onClick={this.handleClick}>
+                {this.state.isOpen ? 'close' : 'open'}
+              </button>
+          </h2>
           {body}
           <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
         </div>
     )
+  }
+
+  handleClick = () => {
+    console.log('---', 'clicked')
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
   }
 }
 
